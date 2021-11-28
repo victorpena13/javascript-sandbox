55e41e4243992f75fb0f74dc8976fe9bd9079d24
victor pena <victor.pena1392@Gmail.com>
2021-11-28 12:10:35 -0600

    created a function that accepts a number through a for loop iteration.

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,2 +1,13 @@
 "use strict";
-console.log('test');
+// Create a function named showMultiplicationTable that
+// accepts a number and console.logs the multiplication
+// table for that number (just multiply by the numbers
+// 1 through 10)
+
+function showMultiplicationTable(number) {
+    for(var i = 1; i < 11; i++) {
+        console.log(i + ' * ' + number + " = " + (i*number));
+    }
+}
+
+console.log(showMultiplicationTable(10));
