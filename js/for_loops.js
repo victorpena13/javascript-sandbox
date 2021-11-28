9dfd6986b15d573ae3efd6fe944eb1b33e31e206
victor pena <victor.pena1392@Gmail.com>
2021-11-28 09:22:26 -0600

    while loop iterating through 1-9.

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,1 +1,22 @@
-console.log("test");
+"use strict";
+
+var i = 0;
+
+while (i < 10) {
+    console.log('while loop iteration #' + i);
+    i++;
+}
+
+//results in the following output:
+//while loop iteration #1
+//while loop iteration #2
+//while loop iteration #3
+//while loop iteration #4
+//while loop iteration #5
+//while loop iteration #6
+//while loop iteration #7
+//while loop iteration #8
+//while loop iteration #9
+//while loop iteration #10
+//if variable i is changed to a number
+//that is equal or greater then 10. the loop will not run
