e4c303792cddc0a0e2adb2565113235bd9f586fd
victor pena <victor.pena1392@Gmail.com>
2021-11-28 12:20:51 -0600

    generated 10 random numbers through a function and for loop to determine whether that number is even or odd.

--- js/for_loops.js
+++ js/for_loops.js
@@ -11,3 +11,21 @@ function showMultiplicationTable(number) {
 }
 
 console.log(showMultiplicationTable(10));
+
+// use a for loop and this snippet of code:
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+}
+// to generate ten random numbers between 20 and 200 and
+// output to the console whether each number is odd or even.
+
+for(var a = 1; a < 11; a++) {
+    var randomInt = getRandomInt(20,200);
+    if (randomInt % 2 === 0) {
+        console.log('the number ' + randomInt + ' is even');
+    } else {
+        console.log('the number ' + randomInt + ' is odd');
+    }
+}
