6872ee8f27f596ed4b4d04e8d706dd428b2388e8
victor pena <victor.pena1392@Gmail.com>
2021-12-08 15:34:23 -0600

    completed all steps of the first problem.  prompted the user for a number 1 through 50 and used a do while loop to make sure the number fit those perameters. and then created a function that would console.log all the odd number 1 through 50 except for the one that the user gave.

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,12 +1,24 @@
 // Prompt the user for an odd number between 1 and 50. Use a loop and a
 // break statement to continue prompting the user if they enter invalid input
 (function() {
+    function oddNumber() {
+        for (var i = 1; i <= 50; i = i + 2) {
+            if (userInput == i) {
+                console.log("Yikes!, lets skip " + userInput);
+                continue;
+            }
+            console.log(i);
+
+        }
+    }
     do {
         var userInput = prompt('enter a number between 1 & 50');
+        if(userInput < 50 && userInput > 1) {
+            console.log(oddNumber(userInput));
+        }
 
-    } while (userInput > 50 || userInput < 1 || isNaN(userInput));
 
-
+    } while (userInput > 50 || userInput < 1 || isNaN(parseInt(userInput)));
 })()
 
 
