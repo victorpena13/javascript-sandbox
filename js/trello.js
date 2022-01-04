62d2e2fd7d3b93c438b8253aeca7768e938da772
victor pena <victor.pena1392@Gmail.com>
2022-01-04 13:03:20 -0600

    created two methods that accomplish the same thing differently one uses findIndex and the other uses find

--- js/trello.js
+++ js/trello.js
@@ -1,5 +1,5 @@
+// simplified version of searching through an array:
 const myTodos = ['Buy Bread', 'Go to Gym', 'Record youtube videos'];
-
 console.log(myTodos.indexOf('Buy Bread'));
 
 
@@ -14,12 +14,36 @@ const newTodos = [{
     isDone: false
 }]
 
-//findIndex will iterate through the newTodos array of objects and return the indice of 'Go to Gym';
+//findIndex will iterate through the newTodos array of objects and return the indice of 'Go to Gym'
+//the findIndex function works for searching through an array holding multiple objects.
+
 const index = newTodos.findIndex(function(todo,index) {
     console.log(todo + " " + index)
     return todo.title === 'Go to Gym';
-})
+});
 
 console.log(index);
 //will result in the details of the object index;
 console.log(newTodos[index]);
+
+// refactored code from lines 20-23 into a function to accept a title object array list to return the index:
+
+const findTodoMethodOne = function(myTodos, title) {
+    const index = myTodos.findIndex(function (todo, index) {
+        return todo.title.toLowerCase() === title.toLowerCase()
+    });
+    return myTodos[index];
+}
+
+let printMe = findTodoMethodOne(newTodos, 'Record youtube videos');
+console.log(printMe);
+
+const findTodoMethodTwo = function(myTodos, title) {
+    const titleReturned = myTodos.find(function(todo, index){
+        return todo.title.toLowerCase() === title.toLowerCase();
+    });
+    return titleReturned;
+}
+
+let printMethodTwo = findTodoMethodOne(newTodos, 'Go to Gym');
+console.log(printMethodTwo);
