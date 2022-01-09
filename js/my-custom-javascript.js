816f451f241cab6148910f4c5466949c6acb85f1
victor pena <victor.pena1392@Gmail.com>
2022-01-08 22:04:48 -0600

    used jQuery to for an h1's background to change colors when clicked

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -1,14 +1,13 @@
 var contents = $('#headline-title').html();
 alert(contents);
 
-$('.dividers').css('border', '1px solid #f00');
+$('h1').click(function(){
+    $('h1').css('background-color', '#f0f0f0');
+});
 
 $('h1, h2').css('color', '#800080');
 
 $('li').css('font-size', '20px');
-
-$(li).css('color', '#FFA500');
-
 
 
 
