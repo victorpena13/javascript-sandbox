401be6a842e1e801a3b594038a43438e5a0bb8b2
victor pena <victor.pena1392@Gmail.com>
2022-01-08 22:14:36 -0600

    completed font size for with dblclick

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -5,6 +5,10 @@ $('h1').click(function(){
     $('h1').css('background-color', '#f0f0f0');
 });
 
+$('p').dblclick(function(){
+    $('p').css('font-size', '18px');
+})
+
 $('h1, h2').css('color', '#800080');
 
 $('li').css('font-size', '20px');
