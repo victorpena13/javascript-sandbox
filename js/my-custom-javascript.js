f8eb46d9ce08173ca45622131248d90a4827d110
victor pena <victor.pena1392@Gmail.com>
2022-01-09 17:19:53 -0600

    added two buttons and used .off and .find to alternate between gray and white backgrounds

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -1,9 +1,12 @@
 var contents = $('#headline-title').html();
 alert(contents);
 
-$('h1').click(function(){
-    $(this).css('background-color', '#f0f0f0');
-});
+// $('h1').click(function(){
+//     $(this).css('background-color', '#f0f0f0');
+// });
+
+
+
 
 $('p').dblclick(function(){
     $(this).css('font-size', '18px');
@@ -22,5 +25,13 @@ $('h1, h2').css('color', '#800080');
 $('li').css('font-size', '20px');
 
 
-
+function grayColor() {
+    $('h1').css('background', '#f0f0f0');
+}
+$('#bind').click(function(){
+    $('body').on('click', '#bind', grayColor)
+});
+$('#unbind').click(function (){
+    $('body').off('click', '#unbind', grayColor).find('h1').css('background', '#ffffff');
+})
 
