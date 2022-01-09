73236af5336498d3409308ae92c237a5348b0f7f
victor pena <victor.pena1392@Gmail.com>
2022-01-08 22:22:47 -0600

    used .hover to execute two functions

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -2,12 +2,20 @@ var contents = $('#headline-title').html();
 alert(contents);
 
 $('h1').click(function(){
-    $('h1').css('background-color', '#f0f0f0');
+    $(this).css('background-color', '#f0f0f0');
 });
 
 $('p').dblclick(function(){
-    $('p').css('font-size', '18px');
-})
+    $(this).css('font-size', '18px');
+});
+
+$('li').hover(
+    function() {
+    $(this).css('color', '#FF0000');
+},
+    function() {
+        $(this).css('color', '#000000')
+    });
 
 $('h1, h2').css('color', '#800080');
 
