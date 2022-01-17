be0e0a9a1d38a81f347ebf06e20fdc795e536383
victor pena <victor.pena1392@Gmail.com>
2022-01-17 16:42:30 -0600

    i used .each to solve these and searched for the first and last element childs and applied css directly to complete the assignment

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -8,3 +8,8 @@ $('h3').click(function (){
     $('ul').children().css('font-weight', 'bold');
 })
 
+$('li').click(function (){
+    $('ul').each(function(index,element){
+        element.firstElementChild.style.color = '#0000FF'
+    })
+})
