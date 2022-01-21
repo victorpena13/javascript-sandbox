cf5ade45f086fccaad70f94dfda9aa0323f5956c
victor pena <victor.pena1392@Gmail.com>
2022-01-20 22:28:56 -0600

    wrote comments to organize the solution for this problem lines 49-51

--- js/edabit-array.js
+++ js/edabit-array.js
@@ -19,9 +19,9 @@ function countTrue(array) {
     return count;
 }
 
-// console.log(countTrue(testArray));
-// console.log(countTrue(testArrayTwo));
-// console.log(countTrue(testArrayThree));
+console.log(countTrue(testArray));
+console.log(countTrue(testArrayTwo));
+console.log(countTrue(testArrayThree));
 
 // Create a function that takes an array of numbers and return both the minimum and maximum numbers, in that order.
 
@@ -41,3 +41,11 @@ function minMax(array) {
 minMax(numbers);
 minMax(numberList);
 minMax(num);
+
+// Create a function that takes a number as an argument.
+//    Add up all the numbers from 1 to the number you passed to the function.
+// For example, if the input is 4 then your function should return 10 because 1 + 2 + 3 + 4 = 10.
+
+//I will make a function that accepts a number
+//and I will count and store each number in an array/variable from 1 till i reach the number passed into the function
+// and then I will take that new array variable and pass it through a for loop to get the sum of all the numbers in the array
