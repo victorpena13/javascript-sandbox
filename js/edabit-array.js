de5e87d033c71e70808061690db094acbfaf77c7
victor pena <victor.pena1392@Gmail.com>
2022-01-20 23:05:25 -0600

    created a function that accepts a number and returns the sum of all the numbers in the array

--- js/edabit-array.js
+++ js/edabit-array.js
@@ -8,7 +8,6 @@ var testArray = [true, false, false, true, false];
 var testArrayTwo = [false, false, false, false];
 var testArrayThree = [];
 
-
 function countTrue(array) {
     var count = 0;
     array.forEach(function(element){
@@ -49,3 +48,22 @@ minMax(num);
 //I will make a function that accepts a number
 //and I will count and store each number in an array/variable from 1 till i reach the number passed into the function
 // and then I will take that new array variable and pass it through a for loop to get the sum of all the numbers in the array
+
+function addUp(number) {
+    var count = []
+    for(var i = 0; i <= number; i++) {
+        count.push(i);
+    }
+    var sum = 0;
+    for(var j = 1; j < count.length; j++) {
+        sum += count[j];
+    }
+    return count + " = " + sum
+}
+
+console.log('test starts here:');
+console.log(addUp(4));
+
+//to solve this problem i had to create a function that accepts a number and used a for loop to iterate and store
+//each number in a variable array.
+//then i created another for loop to iterate through the array i previously created and returned the sum of the array.
