973967e2b412456a139564927743e11d74040723
victor pena <victor.pena1392@Gmail.com>
2022-01-23 12:04:03 -0600

    bonus completed

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -1,11 +1,14 @@
 $(document).ready(function(){
-    $('#render-facts').click(function(e){
+    $('#render-facts-button').click(function(e){
         e.preventDefault();
         $('.invisible').toggleClass('visibility');
-    })
-})
+    });
+});
 
-
+$('dt').click(function(){
+    $("dt").removeClass('selected');
+    $(this).addClass("selected");
+});
 
 $('#change-color').click(function (){
     $('ul').each(function(index, element){
@@ -15,12 +18,12 @@ $('#change-color').click(function (){
 
 $('h3').click(function (){
     $('ul').children().css('font-weight', 'bold');
-})
+});
 
 $('li').click(function (){
     $('ul').each(function(index,element){
         element.firstElementChild.style.color = '#0000FF'
-    })
-})
+    });
+});
 
 
