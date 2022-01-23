641503ed5cc70229c469e1380d7e7a74708117aa
victor pena <victor.pena1392@Gmail.com>
2022-01-23 11:50:43 -0600

    worked with .toggleClass | addClass | removeClass

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -1,3 +1,12 @@
+$(document).ready(function(){
+    $('#render-facts').click(function(e){
+        e.preventDefault();
+        $('.invisible').toggleClass('visibility');
+    })
+})
+
+
+
 $('#change-color').click(function (){
     $('ul').each(function(index, element){
         element.lastElementChild.style.backgroundColor = '#FFFF00';
@@ -13,4 +22,5 @@ $('li').click(function (){
         element.firstElementChild.style.color = '#0000FF'
     })
 })
+
 
