b982b486b79e058c8288b12b3a7d9e42171e3fbe
victor pena <victor.pena1392@Gmail.com>
2022-01-28 12:26:45 -0600

    refactored code to add and turned gecode and marker placement as a reusable function

--- js/mapbox_maps_api.js
+++ js/mapbox_maps_api.js
@@ -1,23 +1,40 @@
 (function (){
-    geocode("4331 McCullough Ave, San Antonio, TX 78212", mapboxAPI_key).then(function(result) {
-        var x = parseFloat(result[0]);
-        var y = parseFloat(result[1]);
-        console.log(x + " " + y);
-        mapboxgl.accessToken = mapboxAPI_key;
-        const map = new mapboxgl.Map({
-            container: 'map', // container ID
-            style: 'mapbox://styles/mapbox/streets-v11', // style URL
-            center: [x,y], // starting position [lng, lat]
-            zoom: 18,// starting zoom
-        });
-        var saborMarker = new mapboxgl.Marker()
-            .setLngLat([x,y])
-            .setPopup(
-                new mapboxgl.Popup({ offset: 25 }) // add popups
-                    .setHTML(`sabor`)
-            )
-            .addTo(map);
+    const restaurants = [
+        {
+            name : "Sabor",
+            address : "4331 McCullough Ave, San Antonio, TX 78212",
+        },
+        {
+            name : "HEB",
+            address : "516 S Flores St, San Antonio, TX 78204",
+        },
+        {
+            name : "Alemeda Cafe",
+            address : "342 W Houston St, San Antonio, TX 78205",
+        },
+]
+    mapboxgl.accessToken = mapboxAPI_key;
+    const map = new mapboxgl.Map({
+        container: 'map', // container ID
+        style: 'mapbox://styles/mapbox/streets-v11', // style URL
+        center: [-98.4916, 29.4252],
+        zoom: 13,// starting zoom
     });
+
+    function placeMarkerAndPopup(info, token, map) {
+        geocode(info.address, token).then(function(coordinates) {
+            var marker = new mapboxgl.Marker()
+                .setLngLat(coordinates)
+                .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
+                        .setHTML(info.name)
+                )
+                .addTo(map)
+        });
+    }
+    placeMarkerAndPopup(restaurants[0], mapboxAPI_key, map);
 }) ();
+
+
+
 
 
