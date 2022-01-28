0467ca9f1481f182e2de77bcff878901d3fe779f
victor pena <victor.pena1392@Gmail.com>
2022-01-27 21:23:58 -0600

    added methods to geocode and reversegeocode

--- js/mapbox_maps_api.js
+++ js/mapbox_maps_api.js
@@ -1,7 +1,22 @@
-mapboxgl.accessToken = mapboxAPI_key;
-const map = new mapboxgl.Map({
-    container: 'map', // container ID
-    style: 'mapbox://styles/mapbox/streets-v11', // style URL
-    center: [-74.5, 40], // starting position [lng, lat]
-    zoom: 9 // starting zoom
-});
+(function (){
+    mapboxgl.accessToken = mapboxAPI_key;
+    const map = new mapboxgl.Map({
+        container: 'map', // container ID
+        style: 'mapbox://styles/mapbox/streets-v11', // style URL
+        center: [-98.4916, 29.4252], // starting position [lng, lat]
+        zoom: 13,// starting zoom
+    });
+
+    var popup = new mapboxgl.Popup()
+        .setLngLat([-98.4921628, 29.4800839])
+        .addTo(map)
+        .setHTML('<h3>sabor</h3>');
+
+    geocode("600 Navarro St #350, San Antonio, TX 78205", mapboxAPI_key).then(function(result) {
+        console.log(result);
+        map.setCenter(result);
+        map.setZoom(20);
+    });
+}) ();
+
+
