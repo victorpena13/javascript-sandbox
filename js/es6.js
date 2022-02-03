88b9f61df826e4db9e9f78a510e43a80c7547b41
victor pena <victor.pena1392@Gmail.com>
2022-02-03 12:25:21 -0600

    introduced let

--- js/es6.js
+++ js/es6.js
@@ -48,9 +48,10 @@ users.push({
 });
 
 
+
 // TODO: replace `var` with `let` in the following variable declarations
-var emails = [];
-var names = [];
+let emails = [];
+let names = [];
 
 // TODO: rewrite the following using arrow functions
 users.forEach(function(user) {
