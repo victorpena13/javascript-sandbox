120667e133d061b440e8eff9b09a42312b4d40a7
victor pena <victor.pena1392@Gmail.com>
2022-02-07 15:50:42 -0600

    introduced - default function parameter values, arrow functions

--- js/es6_lesson.js
+++ js/es6_lesson.js
@@ -35,3 +35,49 @@ const newWay = (name) => `Hello, ${name}!`;
 //parenthesis can be omitted if their is only one argument
 
 
+//default function parameter values
+
+//old way
+
+function sayHello(name) {
+    if (typeof name === 'undefined') {
+        name = 'World';
+    }
+    return 'Hello, ' + name + '!';
+}
+
+sayHello(); // "Hello, World!"
+sayHello('victor') // "Hello, victor!"
+
+//es6 syntax
+
+function sayHi(name = 'World') {
+    return `Hello, ${name}!`;
+}
+
+sayHi(); // "Hello, World!"
+sayHi('victor') // "Hello, victor!"
+
+//arrow functions
+
+const diHola = (nombre = 'World') => `hello, ${nombre}!`;
+
+//object property variable assignment shorthand
+
+//old way
+const nombre = 'victor pena';
+const person = {
+    name: nombre,
+    age: 30,
+}
+
+//new shorthand
+const persona = {
+    nombre,
+    age: 30
+}
+
+//object destructing
+
+
+//old way
