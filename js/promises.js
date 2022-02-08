93642aaa8bf3ad8ccf0d73e8aee32c0d4680db1b
victor pena <victor.pena1392@Gmail.com>
2022-02-08 17:46:22 -0600

    testing last commit -m

--- js/promises.js
+++ js/promises.js
@@ -1,11 +1,10 @@
 const btnRepos = document.getElementById('repoBtn');
-    btnRepos.addEventListener('click', getRepos);
-async function getRepos() {
+    btnRepos.addEventListener('click', getLastCommit);
+async function getLastCommit() {
     const url = 'https://api.github.com/users/victorpena13'
     const response = await fetch(url, {'Authorization': githubAPI_key});
     const result = await response.json()
-
-    console.log(result);
+    console.log(result.updated_at);
 }
 
 
