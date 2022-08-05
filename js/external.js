c221e22e72cdcc4c8950620735f7435c539760e3
Victor Pena <13victor.pena@gmail.com>
2022-08-04 20:21:07 -0500

    && has higher precedence then ||

---
+++ js/external.js
@@ -0,0 +1,9 @@
+console.log(true && true);  //true
+console.log(true && false); //false
+console.log(false && false); // false
+console.log(true || false); //true
+console.log(false || true); //true
+console.log(!true); //false
+console.log(true); //true
+console.log(!false); //true
+console.log(false && false || true); //true
