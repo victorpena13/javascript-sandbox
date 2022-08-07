8569a406c6e181ec678e95df849973ae10b14095
Victor Pena <13victor.pena@gmail.com>
2022-08-06 20:03:29 -0500

    shorthand operations

--- js/variablesLesson.js
+++ js/variablesLesson.js
@@ -2,3 +2,5 @@ var x = 5;
 console.log(x); // 5;
 x = 8;
 console.log(x); //8;
+x = x + x;
+console.log(x); //16
---
+++ js/workingData.js
@@ -0,0 +1,8 @@
+x = 1;
+x = x + 2; // 3
+console.log(x);
+
+// short-hand operations:
+y = 1;
+y += 2; // 3
+console.log(y);
