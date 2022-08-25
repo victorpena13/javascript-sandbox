68c80282f2dc554014febc2ecd4177881a2b5ddf
Victor Pena <13victor.pena@gmail.com>
2022-08-24 23:14:16 -0500

    conditionals.js with analyzeColor()

---
+++ js/conditionals.js
@@ -0,0 +1,19 @@
+function analyzeColor(color) {
+    if (colorPreference === "blue") {
+        alert("That is my favorite!");
+    } else if (colorPreference === "red") {
+        alert("Red is ok...");
+    } else if (colorPreference === "green") {
+        alert("like the Earth!");
+    } else if (colorPreference === "violet") {
+        alert("just like Liz's eyes");
+    } else if (colorPreference === "orange") {
+        alert("like the fruit!");
+    } else if (colorPreference === "yellow") {
+        alert("like the sun!");
+    } else if (colorPreference === "indigo") {
+        alert("I like indigo");
+    } else {
+        alert(colorPreference + ", that is not my favorite, but I like it for you!");
+    }
+}
