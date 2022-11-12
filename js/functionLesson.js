077f0183d470b94e5e459581b21b8b2fb1ce0ecf
Victor Pena <13victor.pena@gmail.com>
2022-11-12 14:04:47 -0600

    immediately invoked function

--- js/functionLesson.js
+++ js/functionLesson.js
@@ -1,72 +1,73 @@
+(function() {
+
+    function sumOfTwo(x,y) {
+        return x + y;
+    }
+
+    console.log(sumOfTwo(2, 4));
+    sumOfTwo(1,3); // will not display to console.
+    console.log(sumOfTwo(9, 9));
+
+    var stringFortyTwo = "42";
+    console.log(typeof stringFortyTwo);
+
+    var fortyTwo = parseInt(stringFortyTwo);
+    console.log(fortyTwo);
 
 
+    function increment(x) {
+        return x+1;
+        console.log("you will not see this line");
+    }
 
-function sumOfTwo(x,y) {
-    return x + y;
-}
+    var four = increment(3);
+    console.log(four);
+    var five = increment(four);
+    console.log(five);
 
-console.log(sumOfTwo(2, 4));
-sumOfTwo(1,3); // will not display to console.
-console.log(sumOfTwo(9, 9));
-
-var stringFortyTwo = "42";
-console.log(typeof stringFortyTwo);
-
-var fortyTwo = parseInt(stringFortyTwo);
-console.log(fortyTwo);
-
-
-function increment(x) {
-    return x+1;
-    console.log("you will not see this line");
-}
-
-var four = increment(3);
-console.log(four);
-var five = increment(four);
-console.log(five);
-
-var six = increment(increment(increment(3)));
-console.log(six);
+    var six = increment(increment(increment(3)));
+    console.log(six);
 
 //anonymous functions
 
-var decrement = function(x) {
-    return x -1;
-}
-var two = decrement(3);
-console.log(two);
+    var decrement = function(x) {
+        return x -1;
+    }
+    var two = decrement(3);
+    console.log(two);
 
-function sayHi() {
-    alert("Hi");
-}
-sayHi();
+    function sayHi() {
+        alert("Hi");
+    }
+    sayHi();
 
-function sayHello(name) {
-    alert("hello " + name.toUpperCase());
-}
+    function sayHello(name) {
+        alert("hello " + name.toUpperCase());
+    }
 
-sayHello("victor");
+    sayHello("victor");
 
 
-var globalVar = "look, i'm global";
+    var globalVar = "look, i'm global";
 
-function alertText() {
-    var localVar = "look, im local";
-    console.log(localVar);
-    alert(globalVar);
-}
+    function alertText() {
+        var localVar = "look, im local";
+        console.log(localVar);
+        alert(globalVar);
+    }
 
-alertText();
+    alertText();
 
-var x = 300;
-var y = 100;
+    var x = 300;
+    var y = 100;
 
-function scopeExample() {
-    var x = 1;
-    var y = 2;
-    console.log('x: ' + x + 'y: ' + y);
-    return x+y;
-}
-scopeExample();
+    function scopeExample() {
+        var a = 1;
+        var b = 2;
+        console.log('x: ' + x + 'y: ' + y);
+        return x+y;
+    }
+    scopeExample();
+
+})();
 
