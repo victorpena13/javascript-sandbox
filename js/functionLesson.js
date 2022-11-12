3b3c690d68ab2e55bf85d1b49623c99b5205d0ee
Victor Pena <13victor.pena@gmail.com>
2022-11-12 14:02:30 -0600

    review of global and local variables

--- js/functionLesson.js
+++ js/functionLesson.js
@@ -1,4 +1,6 @@
 
+
+
 function sumOfTwo(x,y) {
     return x + y;
 }
@@ -17,7 +19,6 @@ console.log(fortyTwo);
 function increment(x) {
     return x+1;
     console.log("you will not see this line");
-
 }
 
 var four = increment(3);
@@ -28,6 +29,44 @@ console.log(five);
 var six = increment(increment(increment(3)));
 console.log(six);
 
+//anonymous functions
+
+var decrement = function(x) {
+    return x -1;
+}
+var two = decrement(3);
+console.log(two);
+
+function sayHi() {
+    alert("Hi");
+}
+sayHi();
+
+function sayHello(name) {
+    alert("hello " + name.toUpperCase());
+}
+
+sayHello("victor");
 
 
+var globalVar = "look, i'm global";
+
+function alertText() {
+    var localVar = "look, im local";
+    console.log(localVar);
+    alert(globalVar);
+}
+
+alertText();
+
+var x = 300;
+var y = 100;
+
+function scopeExample() {
+    var x = 1;
+    var y = 2;
+    console.log('x: ' + x + 'y: ' + y);
+    return x+y;
+}
+scopeExample();
 
